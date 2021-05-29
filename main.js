67e3819e7062717ec85c9c927f6397229941f355
MAJIHYE <majh0294@gmail.com>
2021-05-30 01:47:22 +0900

    completed to Footer [2021-05-30]

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-let nevigation=document.getElementById('Nevigation');
-let header=document.querySelector('header');
-let header_height=header.getBoundingClientRect().height;
-window.onscroll=function(){
-    let window_Y=window.scrollY;
-    if(window_Y > header_height){
-        nevigation.style.opacity=0.9;
-    }
-    else{
-        nevigation.style.opacity=1;
-    }
+// let nevigation=document.getElementById('Nevigation');
+// let header=document.querySelector('header');
+// let header_height=header.getBoundingClientRect().height;
+// window.onscroll=function(){
+//     let window_Y=window.scrollY;
+//     if(window_Y > header_height){
+//         nevigation.style.opacity=0.9;
+//     }
+//     else{
+//         nevigation.style.opacity=0.5;
+//         nevigation.style.zIndex=1;
+//     }
+// }
+
+var nystories = document.querySelector(".profile").offsetTop;
+window.onscroll = function() {
+  if (window.pageYOffset > 0) {
+      var opac = (window.pageYOffset / nystories);
+      document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url('image/background.jpg') no-repeat";
+  }
 }
