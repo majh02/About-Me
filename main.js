c6cc15abc8cdbecbcddb62c2442433a985137357
MAJIHYE <majh0294@gmail.com>
2021-05-30 17:50:01 +0900

    complete background & button event [2021-05-30]

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
-// let nevigation=document.getElementById('Nevigation');
-// let header=document.querySelector('header');
-// let header_height=header.getBoundingClientRect().height;
-// window.onscroll=function(){
-//     let window_Y=window.scrollY;
-//     if(window_Y > header_height){
-//         nevigation.style.opacity=0.9;
-//     }
-//     else{
-//         nevigation.style.opacity=0.5;
-//         nevigation.style.zIndex=1;
-//     }
-// }
-
 var nystories = document.querySelector(".profile").offsetTop;
-window.onscroll = function() {
-  if (window.pageYOffset > 0) {
-      var opac = (window.pageYOffset / nystories);
-      document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url('image/background.jpg') no-repeat";
-  }
+var nevigation = document.getElementById('Nevigation');
+var top_btn=document.getElementById('top_btn');
+var header=document.querySelector('header').offsetTop;
+var footer=document.querySelector('footer').offsetTop;
+
+window.onscroll = function(){
+    // Page Fadeout
+    if (window.pageYOffset > 0) {
+        var opac = (window.pageYOffset / nystories);
+        document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url('image/background.jpg') no-repeat";
+    }
+    // Nevigation & TopButton
+    if(window.pageYOffset >= header){
+        nevigation.style.background="rgb(238, 136, 170, 0.9)";
+    }
+    else if(window.pageYOffset < header){
+        nevigation.style.background="none";
+        top_btn.style.visibility="hidden";
+    }
+    else{
+        top_btn.style.visibility="visible"
+    }
 }
+
+var buttonArr = document.querySelectorAll('#Nevigation .menu button');
+for(let i=0; i<buttonArr.length; i++){
+    buttonArr[i].addEventListener('click',()=>{
+        document.getElementById('title'+(i+1)).scrollIntoView(true);
+    });
+}
+top_btn.addEventListener('click',()=>{
+    document.getElementById('title1').scrollIntoView(true);
+});
